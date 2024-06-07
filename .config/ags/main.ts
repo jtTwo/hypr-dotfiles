526fc18eecdbdda56c5c6bd8b8bc3584d56ad491
pc_master <dotfiles.lullaby060@passinbox.com>
2024-06-07 06:55:32 +0400

    ags nvim test

--- .config/ags/main.ts
+++ .config/ags/main.ts
@@ -4,52 +4,57 @@ import "lib/session"
 import options from "options"
 import { setupDateMenu } from "widget/datemenu/dateMenu"
 
-const win = Widget.Window({
-    name: "mpris",
-    class_name: "player1",
-    anchor: ["top", "left"],
-    child: Media(),
+const wind = Widget.Window({
+  name: "ddd",
 })
-const win2 = Widget.Window ({
-	name: "mpris2",
-	visible: false,
-	anchor: ["bottom", "left"],
-	child: Media(),
+
+const win = Widget.Window({
+  name: "mpris",
+  class_name: "player1",
+  anchor: ["top", "left"],
+  child: Media(),
+})
+
+const win2 = Widget.Window({
+  name: "mpris2",
+  visible: false,
+  anchor: ["bottom", "left"],
+  child: Media(),
 })
 
 //bar??
 const time = Variable('', {
-    poll: [1000, function() {
-        return Date().toString();
-    }],
+  poll: [1000, function () {
+    return Date().toString();
+  }],
 });
 
 const Bar = (monitor: number) => Widget.Window({
-    monitor,
-    name: `bar${monitor}`,
-    class_name: "bar",
-    anchor: ['top', 'left', 'right'],
-    exclusivity: 'exclusive',
-    child: Widget.CenterBox({
-        start_widget: Widget.Label({
-            hpack: 'center',
-            label: 'Welcome to AGS!',
-        }),
-        end_widget: Widget.Label({
-            hpack: 'center',
-            label: time.bind(),
-        }),
+  monitor,
+  name: `bar${monitor}`,
+  class_name: "bar",
+  anchor: ['top', 'left', 'right'],
+  exclusivity: 'exclusive',
+  child: Widget.CenterBox({
+    start_widget: Widget.Label({
+      hpack: 'center',
+      label: 'Welcome to AGS!',
     }),
+    end_widget: Widget.Label({
+      hpack: 'center',
+      label: time.bind(),
+    }),
+  }),
 });
 
 App.config({
-    onConfigParsed: () => {
-	    setupDateMenu()
-    },
-    style: "./style.css",
-    windows: [
-	win,
-	win2,
-    // Bar(0),
-    ],
+  onConfigParsed: () => {
+    setupDateMenu()
+  },
+  style: "./style.css",
+  windows: [
+    win,
+    win2,
+    Bar(0),
+  ],
 })
